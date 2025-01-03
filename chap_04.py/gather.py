3be5b70a73f9f52ed44745fb982249bb38edc50b
etrazia17 <etrazia02102317@gmail.com>
2025-01-04 03:10:28 +0500

    Gather

--- chap_04.py/gather.py
+++ chap_04.py/gather.py
@@ -1,16 +1,16 @@
 from mpi4py import MPI
-comm = MPI.COMM_WORLD
 
+comm = MPI.COMM_WORLD
 size = comm.Get_size()
 rank = comm.Get_rank()
 
-data = (rank+1)**2
-data = comm.gather(data, root=0)
+data = (rank + 1) ** 2
+
+gathered_data = comm.gather(data, root=0)
 
 if rank == 0:
-    print ("rank = %s " %rank +\
-          "...receiving data to other process")
-for i in range(1,size):
-       value = data[i]
-       print(" process %s receiving %s from process %s"\
-            %(rank , value , i))
+    print(f"rank = {rank} ...receiving data from other processes")
+    
+    for i in range(1, size):  
+        value = gathered_data[i]  
+        print(f"process {rank} receiving {value} from process {i}")
