935c6f39436447fa22c688fc4ab4304c679a2e59
etrazia17 <etrazia02102317@gmail.com>
2025-01-04 02:15:24 +0500

    Scatter

---
+++ chap_04.py/scatter.py
@@ -0,0 +1,13 @@
+from mpi4py import MPI
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+size = comm.Get_size()
+
+if rank == 0:
+    array_to_share = [1, 2, 3, 4 ,5 ,6 ,7, 8 ,9 ,10]
+    
+else:
+    array_to_share = None
+
+recvbuf = comm.scatter(array_to_share, root=0)
+print("Process = %d, recvbuf = %d" % (rank, recvbuf))
